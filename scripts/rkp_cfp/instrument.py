dd77913f0b6797690ebf7cdc95a63e2eaf35486a
huangkaikaihuang <52343835+huangkaikaihuang@users.noreply.github.com>
2023-10-13 17:46:55 +0800

    Update instrument.py

--- scripts/rkp_cfp/instrument.py
+++ scripts/rkp_cfp/instrument.py
@@ -1094,7 +1094,7 @@ def parse_sections(vmlinux):
                 rb'\s+(?P<lma>{hex_re})'
                 rb'\s+(?P<offset>{hex_re})'
                 rb'\s+(?P<align>[^\s]+)'
-            ).format(hex_re=hex_re.encode()), line)
+            ).decode().format(hex_re=hex_re.decode()), line)
         if m:
             section = {}
 
